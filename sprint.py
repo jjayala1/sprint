671e8a8e30bcd25e16bbd7c544407d8d25f71736
Jesus Juarez <jjayala1@gmail.com>
2021-10-20 23:18:33 -0500

    sevral changes

--- sprint.py
+++ sprint.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
@@ -7,9 +8,12 @@ import datetime
 
 class Sprint():
 
-    def __init__(self, user=''):
-        self.user = user
-        self.conn = self.create_database('/home/sprintOct21/sprint/sprint.db');
+    def __init__(self, day=10, file=''):
+        self.day = day
+        self.file = file
+        self.ruta = '/home/jjayala/kit/sprint'
+        #self.ruta = '/home/sprintOct21/sprint');
+        self.conn = self.create_database(f'{self.ruta}/sprint.db');
         self.create_tables()
 
         #self.link = 'https://www.linkedin.com/posts/gonzalo-career-strategist_jobs-careers-jobseekers-activity-6854777607670632448-MiOP/'
@@ -20,8 +24,8 @@ class Sprint():
 
     def main(self):
 
-        if self.user != '':
-            self.read_file(self.user)
+        if self.file != '':
+            self.read_file()
         elif self.link != '':
             self.get_page()
 
@@ -33,16 +37,18 @@ class Sprint():
             self.get_comments()
 
     def get_page(self):
-        page = requests.get(self.link)
+        #page = requests.get(self.link)
         #with open('/home/sprintOct21/sprint/file.txt', 'bw') as file:
         #    file.write(page.content)
-        self.data = page.content
+        #self.data = page.content
+        pass
 
     def read_file(self):
-        f = open(f'{self.user}.html');
+        f = open(f'{self.ruta}/posts/{self.file}');
         self.data = f.read()
 
     def get_likes(self):
+        self.link = self.soup.find("meta", property="og:url")['content']
         self.owner = self.soup.find('a', class_ = 'share-update-card__actor-text-link').text.strip()
         self.num_likes = self.soup.find('span', class_ = 'social-counts-reactions__social-counts-numRections').text.strip()
         #num_comments = self.soup.find_all('a', { 'class':'social-action-counts__social-counts-item'})
@@ -58,9 +64,16 @@ class Sprint():
         x = datetime.datetime.now()
         curtime = x.strftime("%Y-%m-%d %X")
 
-        sql = 'REPLACE INTO posts (day, owner, link, start_date, update_date, num_comments, num_likes) VALUES(?,?,?,?,?,?,?);'
+        get_id = f"SELECT id from posts where link='{self.link}'"
+        curid = self.conn.cursor()
+        print(get_id, (self.link))
+        curid.execute(get_id)
+        qid = curid.fetchone()
+        id = int(qid[0])
+
+        sql = 'REPLACE INTO posts (id, day, owner, link, start_date, update_date, num_comments, num_likes) VALUES(?,?,?,?,?,?,?,?);'
         cur = self.conn.cursor()
-        cur.execute(sql, (self.day, self.owner, self.link, curtime, curtime, self.num_comments, self.num_likes))
+        cur.execute(sql, (id, self.day, self.owner, self.link, curtime, curtime, self.num_comments, self.num_likes))
         self.conn.commit()
         return cur.lastrowid
 
@@ -69,6 +82,11 @@ class Sprint():
 
         comments = self.soup.find_all('section', class_ = 'comment')
         print(len(comments))
+
+        cur = self.conn.cursor()
+        sql_delete = f"DELETE FROM comments where id_post='{self.id_post}';"
+        cur.execute(sql_delete)
+        self.conn.commit()
 
         print('***************************************************************************')
         print('*******************************MESSAGES************************************')
@@ -81,10 +99,9 @@ class Sprint():
         print('***************************************************************************')
 
     def insert_comments(self):
-
-        sql = 'INSERT INTO comments(id_post, author, message) VALUES(?,?,?);'
         cur = self.conn.cursor()
-        cur.execute(sql, (self.id_post, self.author, self.message))
+        sql_insert = 'INSERT INTO comments(id_post, author, message) VALUES(?,?,?);'
+        cur.execute(sql_insert, (self.id_post, self.author, self.message))
         self.conn.commit()
         return cur.lastrowid
 
@@ -133,7 +150,7 @@ class Sprint():
 
     def pivot_table(self, day='%', owner='%'):
 
-        sql_sprinters = f"SELECT sprinter from sprinters order by sprinter" 
+        sql_sprinters = f"SELECT sprinter from sprinters order by sprinter"
         spr = self.conn.cursor()
         spr.execute(sql_sprinters)
         sprinters = []
@@ -150,7 +167,7 @@ class Sprint():
             sql_pivot += f"sum(case when C.author='{s[0]}' then 1 end) '{s[0]}', "
             sprinters.append(s[0])
 
-        sql_pivot += f"""count(C.id) Total from posts P inner join comments C on P.id=C.id_post 
+        sql_pivot += f"""count(C.id) Total from posts P inner join comments C on P.id=C.id_post
                         where day like '{day}' and owner like '{owner}'
                         group by P.id
                         order by P.owner;
@@ -178,8 +195,16 @@ class Sprint():
 
 
 if __name__ == '__main__':
-    sprint = Sprint()
-    sprint.main()
-    print(sprint.pivot_table())
+
+    for file_post in os.scandir("./posts"):
+        if file_post.name[-4:] == 'html':
+            print(file_post.name)
+            origin = file_post.name
+            dest = file_post.name + '.proc'
+
+            sprint = Sprint(16, f'{origin}')
+            sprint.main()
+            print(sprint.pivot_table())
+            os.rename(f'./posts/{origin}', f'./posts/{dest}')
 
 
